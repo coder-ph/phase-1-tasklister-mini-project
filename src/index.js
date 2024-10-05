3a8de268db36bc657ec729bc8adc45e24e8f8f40
Phelix <phelixmbani@gmail.com>
2024-10-06 01:22:12 +0300

    project DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  const taskForm = document.getElementById('create-task-form');
+  
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+
+    const taskDescription = e.target.newTaskDescription.value;
+
+    printOutPut(taskDescription);
+    taskForm.reset();
+  });
 });
+
+function printOutPut(output) {
+  let ulList = document.querySelector('#tasks');
+  let listItem = document.createElement('li');
+  let deleteBtn = document.createElement('button');
+
+  deleteBtn.addEventListener('click', handleDelete);
+  deleteBtn.textContent = 'x';
+
+  listItem.textContent = output + " ";
+  listItem.appendChild(deleteBtn);
+  
+  ulList.appendChild(listItem);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
